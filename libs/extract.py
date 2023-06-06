3e97d53ecb4b869f4643849c3db296217f9b446b
Cuong Nguyen <cuongnguyen.work8797@gmail.com>
2023-06-06 23:34:21 +0700

    init production

---
+++ libs/extract.py
@@ -0,0 +1,30 @@
+import re
+from collections import Counter
+from config import *
+from libs.helper import *
+
+class ExtractFeatures:
+    
+    def __init__(self, file, export_to) -> None:
+        self.resource = ROOT_DIR + file
+        self.export_to = ROOT_DIR + export_to
+        self._sensitive_functions = ['exec', 'shell_exec', 'passthru', 'system', 'show_source', 'proc_open', 'pcntl_exec', 'eval', 'assert']
+        self._df = None
+
+    def extract_function_names(self):
+        filter = [
+            'if', 'while', 'die', 'elseif', 'go', 'back', 'sort', 'foreach', 'switch', 'trim', 'array', 'count',
+            'session_start', 'header', 'try', 'catch', 'floor', 'round', 'max', 'min', 'session_destroy', 'css', 'pagination',
+            'add', 'pop', 'shift', 'edit', 'int32', 'end', 'prev','next', 'for'
+        ]
+        regex = re.compile(r"(?<=[\s|\;|\:|\=|\-|\+|\]|\[|\`|\.|\<])[a-zA-Z][a-zA-Z0-9\_]{1,255}(?=\()")
+        with open(self.resource, 'r', encoding='ISO-8859-1') as file:
+          results = [x.lower() for x in regex.findall(file.read())]
+          return Counter([result for result in results if result not in filter])
+    
+    def extract_entropy_file(self):
+        return calc_entropy(self.resource)
+
+    def extract_by_word2vec(self):
+        return None
+    
---
+++ libs/helper.py
@@ -0,0 +1,33 @@
+import math
+
+"""
+  Calculate entropy a file as feature of webshell detection method
+  References:
+    - https://www.linkedin.com/pulse/hunting-base64-php-webshells-shannon-entropy-technique-amir-rasa/?trk=articles_directory
+    - https://stackoverflow.com/questions/990477/how-to-calculate-the-entropy-of-a-file
+"""
+def calc_entropy(file):
+    entropy = 0.0
+    with open(file, 'rb') as f:
+      data = f.read()
+      possible = dict(((chr(x), 0) for x in range(0, 256)))
+
+      for byte in data:
+          possible[chr(byte)] +=1
+
+      data_len = len(data)
+
+      # compute
+      for i in possible:
+          if possible[i] == 0:
+              continue
+
+          p = float(possible[i] / data_len)
+          entropy -= p * math.log(p, 2)
+    return entropy
+
+"""
+  This feature only affected to specific language when extracting feature
+"""
+def sensitive_functions():
+    pass
